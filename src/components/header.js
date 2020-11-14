de956b2c2b1d2a8ea2092a5bf802b269e59b7ba4
Eunji Lee <eunjilee@eunjiui-MacBook-Pro-93.local>
2020-11-14 16:39:03 +0900

    logo #1

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import './Header.css'
+import logo from '.././assets/logo/logo_fall@3x.png'
 
 const Header = () => {
     return(
         <div className = "header-container">
-           <h3 className = "logo">LOGO</h3>
+           <img src={logo} alt="logo fall" className = "logo"/>
            <div className="header-nav">
                 <Link className="nav-1" to = "/Ranking">이화랭킹</Link>
                 <Link className="nav-2" to = "/Community">커뮤니티</Link>
