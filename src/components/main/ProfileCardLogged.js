09714fe915275ebfe456a6213a515d671135af2b
Eunji Lee <eunjilee@eunjiui-MacBook-Pro-93.local>
2020-12-04 00:48:28 +0900

    main page logged fix #5

--- src/components/main/ProfileCardLogged.js
+++ src/components/main/ProfileCardLogged.js
@@ -8,23 +8,23 @@ const ProfileCardLogged = () => {
             <div className = "username-profile">Jyami</div>
             <div className = "info-detail-profile">Backend Developer</div>
             <div className = "profile-rowContainer"> 
-                <div>
-                    <div>EWHA RANKING 12등/1020점</div>
+                <div className = "itembox-profile">
+                    <div className = "subtitle-profile">EWHA RANKING 12등/1020점</div>
                     <div className = "profile-rowContainer-with-border">
                         <div className = "profile-box-with-border">
-                            <div>총 커밋</div>
-                            <div>210</div>
+                            <div className = "commit-profile">총 커밋</div>
+                            <div className = "number-profile">210</div>
                         </div>
                         <div className = "profile-box-no-border">
-                            <div>연속 커밋</div>
-                            <div>23</div>
+                            <div className = "commit-profile">연속 커밋</div>
+                            <div className = "number-profile">23</div>
                         </div>
                     </div>
                 </div>
 
-                <div>
-                    <div>커밋동산</div>
-                    <div>273 contributions last year</div>
+                <div className = "itembox2-profile">
+                    <div className = "subtitle-profile">커밋동산</div>
+                    <div className = "contribution-profile">273 contributions last year</div>
                     <div className = "profile-graybox"></div>
                 </div>
                 
