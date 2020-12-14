feba74d6ef928b9c0344184cd20b102310e0ebd5
mjung1798 <mor2222@naver.com>
2020-12-14 19:22:38 +0900

    Feat: hashRouter => BrowserRouter

--- src/auth/OAuth2RedirectHandler.js
+++ src/auth/OAuth2RedirectHandler.js
@@ -8,7 +8,6 @@ class OAuth2RedirectHandler extends Component {
     getUrlParameter(name) {
         name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
         var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-
         var results = regex.exec(this.props.location.search);
         return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
     };
