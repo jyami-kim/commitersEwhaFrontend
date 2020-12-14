c8a7bb24aab7c5a9a9c27ed54cc4f95d0d3eead0
mjung1798 <mor2222@naver.com>
2020-12-14 16:00:39 +0900

    Fix: constants change

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ export const API_BASE_URL = 'http://ewha-commiters-server.herokuapp.com';
 export const ACCESS_TOKEN = 'accessToken';
 
 // export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
-export const OAUTH2_REDIRECT_URI = 'https://mjung1798.github.io/commitersEwhaFrontend/oauth2/redirect'
+export const OAUTH2_REDIRECT_URI = 'http://ewha-commiters.herokuapp.com/oauth2/redirect'
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 
